a2a3090571c26d6267e7fc2ac96ee47b634c559c
qoo12 <121751817+qoo12@users.noreply.github.com>
2023-03-21 15:41:38 +0900

    revised

--- codetest_H_index.py
+++ codetest_H_index.py
@@ -2,7 +2,7 @@ def solution(citations):
     answer = 0
     citations.sort(reverse = True)
     for i in range(0, len(citations)) :
-        if i+1 >= citations[i] :
-            answer = citations[i]
+        if i+1 <= citations[i] :
+            answer = i+1
             break
     return answer
